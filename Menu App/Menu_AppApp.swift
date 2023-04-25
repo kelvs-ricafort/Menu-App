3ab7c8d69e5811f06d44e1e750dbbc82e3bdbb17
Kelvin C. Ricafort <kelvs.r@outlook.com>
2023-04-25 16:35:04 +0800

    Initial Commit

---
+++ Menu App/Menu_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Menu_AppApp.swift
+//  Menu App
+//
+//  Created by Kelvin on 4/25/23.
+//
+
+import SwiftUI
+
+@main
+struct Menu_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
