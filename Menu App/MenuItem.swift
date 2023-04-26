38689ae82fc02a66bcb80cb09a120807ad972bdf
Kelvin C. Ricafort <kelvs.r@outlook.com>
2023-04-26 16:23:32 +0800

    Initial Commit

---
+++ Menu App/MenuItem.swift
@@ -0,0 +1,17 @@
+//
+//  MenuItem.swift
+//  Menu App
+//
+//  Created by Kelvin on 4/25/23.
+//
+
+import Foundation
+
+struct MenuItem: Identifiable {
+    var id: UUID = UUID()
+    var name: String
+    var price: String
+    var imageName: String
+    
+    
+}
